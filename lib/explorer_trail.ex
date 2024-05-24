6ae1adf26aa113fd8c35a47a542a2e91797db59b
puneet.bhasin <puneetsbhasin@gmail.com>
2024-05-24 22:04:03 +0530

    init

---
+++ lib/explorer_trail.ex
@@ -0,0 +1,18 @@
+defmodule ExplorerTrail do
+  @moduledoc """
+  Documentation for `ExplorerTrail`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExplorerTrail.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/explorer_trail_test.exs
@@ -0,0 +1,8 @@
+defmodule ExplorerTrailTest do
+  use ExUnit.Case
+  doctest ExplorerTrail
+
+  test "greets the world" do
+    assert ExplorerTrail.hello() == :world
+  end
+end
